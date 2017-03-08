c722f895991b112863b0774dfe6cb75b3c788135
LKay <lapkom@gmail.com>
2017-03-08 16:27:59 +0900

    Ad missing methods to reporter, simplify interfaces

--- src/reporter.ts
+++ src/reporter.ts
@@ -2,119 +2,184 @@ import { EventEmitter } from "events"
 import { EOL } from "os"
 import * as process from "process"
 
-interface WDIORunner {
-    browserName: string
+// This interfaces are used only internally and don't reflect whole data shape
+
+type WDIOTestState = "pass" | "pending" | "fail"
+
+interface WDIOError {
+    type?: string
+    message?: string
+    stack?: string
 }
 
-interface WDIOReporterRun {
-    capabilities: any[]
-    config: {}
-}
-
-interface WDIOReporterSuite {
-    title: string
-    parent: string
-    fullTitle: string
-    pending: false
-    file: string
-    cid: string
-    specs: string[]
+interface WDIOBase {
     event: string
-    runner: { [cid: string]: WDIORunner }
+    cid: string
     uid: string
     parentUid: string
-    specHash: string
+    specHash?: string
+    specs?: string[]
 }
 
-interface WDIOReporterTest {
-
+interface WDIOResultBase extends WDIOBase {
+    title: string
+    pending: boolean
+    file: string
+    start: number
+    end: number
+    duration: number
 }
 
-interface WDIOReporterHook {
+type WDIOReporterSuite = WDIOResultBase
 
+interface WDIOReporterTest extends WDIOResultBase {
+    state: WDIOTestState
+    error?: WDIOError
 }
 
-function registerSuite () {
-
+interface Results {
+    pass: number
+    skip: number
+    fail: number
+    tests: number
+    done: number
 }
 
 class TapReporter extends EventEmitter {
 
-    private baseReporter;
+    public static out (chunk: string): void {
+        process.stdout.write(chunk)
+        process.stdout.write(EOL)
+    }
 
-    private config;
+    protected baseReporter: any
+    protected config: any
+    protected options: any
+    protected suites: WDIOReporterSuite[] = []
+    protected results: Results
 
-    private options;
-
-    private suites: WDIOReporterSuite[] = []
-
-    private pass: number = 0
-    private skip: number = 0
-    private fail: number = 0
-    private tests: number = 0
-
-    constructor (baseReporter, config, options = {}) {
+    constructor (baseReporter: any, config: any, options?: any) {
         super()
 
         this.baseReporter = baseReporter
         this.config = config
         this.options = options
+        this.results = {
+            pass   : 0,
+            skip   : 0,
+            fail   : 0,
+            tests  : 0,
+            done   : 0
+        }
 
-        this.suites = []
+        this.on("start", () => TapReporter.out("TAP version 13"))
+        this.on("suite:start", (suite: WDIOReporterSuite) => this.suites.push(suite))
+        this.on("test:start", () => this.results.tests++)
+        this.on("test:end", this.onTestResult)
+        this.on("test:fail", this.onTestResult)
+        this.on("test:pending", this.onTestResult)
 
-        this.on("start", (run: WDIOReporterRun) => {
-            TapReporter.println("TAP version 13")
-        })
+        this.on("end", () => {
+            const {
+                fail,
+                pass,
+                skip,
+                tests
+            } = this.results
+            const { start, end } = this.baseReporter.stats
 
-        this.on("suite:start", (suite: WDIOReporterSuite) => {
-            this.suites = this.suites.concat([suite])
-        })
+            if (tests === 0) {
+                TapReporter.out("1..0 # SKIP No tests present")
+            }
 
-        /*
-        this.on("hook:start", (a, b, c) => {
-            console.warn("hook:start", a, b, c)
-        })
+            const lines: string[] = [
+                `1..${tests}`,
+                `# tests ${tests}`,
+                `# pass ${pass}`,
+                `# skip ${skip}`,
+                `# fail ${fail}`,
+                `# Finished in ${end - start}ms`
+            ]
 
-        this.on("hook:end", (a, b, c) => {
-            console.warn("hook:end", a, b, c)
-        })
-        */
-
-        this.on("test:start", (test: WDIOReporterTest) => {
-            this.tests += 1
-            //console.warn("test:start", test)
-        })
-
-        this.on("test:end", (test: WDIOReporterTest) => {
-            this.pass += 1
-            //console.warn("test:end", test)
-        })
-
-        this.on("test:fail", (test: WDIOReporterTest) => {
-            this.fail += 1
-            //console.warn("test:fail", test)
-        })
-
-        this.on("test:pending", (test: WDIOReporterTest) => {
-            this.skip += 1
-            //console.warn("test:pending", test)
-        })
-
-        this.on("suite:end", (suite: WDIOReporterSuite) => {
-            //console.warn("suite:end", suite)
-        })
-
-        this.on("end", (run: WDIOReporterRun) => {
-            TapReporter.println(`1..${this.tests}`)
-            TapReporter.println(`# tests ${this.tests}`)
-            TapReporter.println(`# pass ${this.pass}`)
-            TapReporter.println(`# skip ${this.skip}`)
-            TapReporter.println(`# fail ${this.fail}`)
+            TapReporter.out(lines.join(EOL))
         })
     }
 
-    public static println (message: string): void {
-        process.stdout.write(`${message}${EOL}`)
+    private onTestResult = (test: WDIOReporterTest) => {
+        this.results.done++
+
+        const testTitle: string = this.getTestPath(test)
+        const description: string = `# [Runner: ${test.cid}] ${testTitle} (${test.duration}ms)`
+        const lines: string[] = [description]
+        const { done } = this.results
+
+        switch (test.state) {
+            case "pass":
+            case "pending":
+                if (test.pending) {
+                    this.results.skip++
+                } else {
+                    this.results.pass++
+                }
+
+                const directive: string = test.pending ? " # SKIP Test skipped" : ""
+                lines.push(`ok ${done} - ${testTitle}${directive}`)
+                break
+
+            case "fail":
+                this.results.fail++
+                const error: WDIOError = test.error || {}
+
+                lines.push(`not ok ${done} - ${testTitle}`)
+                lines.push("# Diagnostics")
+                lines.push("  ---")
+                lines.push(`  message: ${error.message || "Test failed without message"}`)
+                lines.push("  severity: fail")
+                lines.push("  data:")
+                lines.push(`    file: ${test.file}`)
+                if (error.type) {
+                    lines.push(`    type: ${error.type}`)
+                }
+                if (error.stack) {
+                    lines.push(`    stack: ${error.stack}`)
+                }
+                lines.push("  ...")
+                break
+
+            default:
+                lines.push(`Bail out! Test ${done} has incorrect state and could not be processed`)
+        }
+
+        TapReporter.out(lines.join(EOL))
+    }
+
+    protected getTestPath (test: WDIOReporterTest): string {
+        if (test.uid === test.parentUid) {
+            return test.title
+        }
+
+        const parents: string[] = []
+        const path: string[] = [test.title]
+        let parentUid = test.parentUid
+
+        while (parentUid !== null) {
+            const parent: WDIOReporterSuite = this.suites.find((suite: WDIOReporterSuite) => suite.uid === parentUid)
+
+            if (parent) {
+                parents.push(parent.uid)
+                path.push(parent.title)
+                parentUid = parent.parentUid
+            } else {
+                parentUid = null
+            }
+
+            // To prevent infinite loop
+            if (parents.indexOf(parentUid) !== -1) {
+                parentUid = null
+            }
+        }
+
+        return path.reverse().join(" \u203A ")
     }
 
 }
